15dd8c49ef96590c497e3a363982fa95be55ca0f
arifusinbad <ariful.islam.sinbad@gmail.com>
2022-09-29 18:32:23 +0600

    commit 8

--- src/component/Study.js
+++ src/component/Study.js
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from 'react';
+import Time from './addTime/Time';
 import './Study.css'
 import Read from './study/Read';
 
 const Study = () => {
  const [study, setStudy] = useState([]);
+ const [time, setTime] = useState([])
  // console.log(study)
  useEffect(() =>{
   fetch('islamic.json')
   .then(res => res.json())
   .then(data => setStudy(data))
  }, [])
+
 const handleAdd = (study) =>{
- console.log(study)
+const newAdd =[...time, study];
+setTime(newAdd)
 }
+const studies = study.filter(x => x.lenght < 4)
+// console.log(studies)
+let total = 0;
+time.forEach(times =>{
+total = total + parseFloat(times.time);
+
+})
+const times = parseFloat(total);
 
  return (
   <div className='study-c'>
@@ -26,6 +38,31 @@ const handleAdd = (study) =>{
    }
    </div>
    <div className='container2'>
+<div className='cart'> 
+ <h3>Sinbad Hossain</h3>
+ <div className='myInfo'>
+  <span>
+   <p>53kg</p>
+   <small>weight</small>
+  </span>
+  <span>
+   <p>5.3</p>
+   <small>Height</small>
+  </span>
+  <span>
+   <p>23yrs</p>
+   <small>Age</small>
+  </span>
+ </div>
+ <h4>Add A Break</h4>
+</div>
+
+<p>Study Time: {times}min</p>
+  {
+   studies.map(studi => <Time
+   studi={studi}
+   ></Time> )
+  }
 
    </div>
   
---
+++ src/component/addTime/Time.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './time.css'
+const Time = ({studi}) => {
+ return (
+  <div>
+   <h5>sfafafa</h5>
+  </div>
+ );
+};
+
+export default Time;
